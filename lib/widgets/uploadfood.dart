5b70893819aef8a2de392be94c300a82edef9bb9
Syed Abdullah <syedabdullah@Syeds-MacBook-Air.local>
2024-11-15 19:16:35 +0530

    update

--- lib/widgets/uploadfood.dart
+++ lib/widgets/uploadfood.dart
@@ -1,24 +1,15 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:foodex/event_details.dart';
 
 class HomeScreen extends StatefulWidget {
-     HomeScreen({
-    required this.shopname,
-    required this.address,
-    required this.shopnumber,
-    required this.name,
-    required this.imagePath
-  });
-
+  HomeScreen({required this.shopname, required this.address, required this.shopnumber, required this.name, required this.imagePath});
 
   String shopname;
   String address;
   String shopnumber;
   String name;
   String imagePath;
-
-
-
 
   @override
   State<HomeScreen> createState() => _UploadfoodState();
@@ -27,7 +18,9 @@ class HomeScreen extends StatefulWidget {
 class _UploadfoodState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(floatingActionButton: FloatingActionButton(backgroundColor: const Color.fromARGB(248, 213, 108, 108),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color.fromARGB(248, 213, 108, 108),
         onPressed: () {
           Navigator.push(
               context,
@@ -38,63 +31,60 @@ class _UploadfoodState extends State<HomeScreen> {
                         address: widget.address,
                       )));
         },
-        child: const Icon(Icons.fastfood_outlined,color: Colors.white,),
-      ),      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-
-      body
-      : Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Row(
-      
-                ),
-                Container(
-                  width: 120,
-                  height: 120,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: AssetImage('assets/userPhoto/A2B.jpg'), // Add your image path
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                 Text(
-                widget.shopname,
-                  style: const TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 8),
-                 Text(
-                  widget.address,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey,
-                  ),
-                ),
-                const SizedBox(height: 8),
-                 Text(
-                  widget.shopnumber,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey,
-                  ),
-                ),
-                const SizedBox(height: 8),
-                 Text(
-                  widget.name,
-                  style: const TextStyle(
-                    fontSize: 14,
-                    color: Colors.grey,
-                  ),
-                ),
-              ],
+        child: const Icon(
+          Icons.fastfood_outlined,
+          color: Colors.white,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              child: CachedNetworkImage(
+                imageUrl: widget.imagePath,
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              ),
             ),
-          ),
+            const SizedBox(height: 20),
+            Text(
+              widget.shopname,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              widget.address,
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              widget.shopnumber,
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              widget.name,
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.grey,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
